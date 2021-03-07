f094c49e8db084dd9cbfd2229d9e2b421d860499
Vikas Kumar <vikas9.js@gmail.com>
2021-03-08 00:07:21 +0530

    Added tailwindcss

---
+++ database/migrations/2021_02_21_122832_create_user_types_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserTypesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_types', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable(false);
+            $table->timestamps();
+        });
+
+        DB::table('user_types')->insert(
+            ['name' => 'admin'],
+            ['name' => 'creator'],
+            ['name' => 'viewer'],
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_types');
+    }
+}
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  urge: [
+    './resources/**/*.blade.php',
+    './resources/**/*.js',
+    './resources/**/*.vue',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
